52a60c71eb6a3101b4e3029babd916809239dcb6
Karim-53 <mohamedkarim.belaid@supcom.tn>
2026-09-20 12:46:24 +0200

    Add: search-in-rotated-sorted-array-ii - submission-1

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        def _bisect(left= 0, right=len(nums) - 1):
+        
+            if not (left <= right):return False
+            mid = (left + right) // 2
+            
+            if nums[mid] == target:
+                return True
+            # Check if the left half is normally sorted
+            if nums[left] < nums[mid]:
+                # Check if target lies within the sorted left half
+                if nums[left] <= target < nums[mid]:
+                    return _bisect(left=left, right = mid - 1)  # Go left
+                else:
+                    return _bisect(left = mid + 1,right=right)   # Go right
+            # Otherwise, the right half must be normally sorted
+            elif nums[mid] < nums[right]: # Check if the left half is normally sorted
+                # Check if target lies within the sorted right half
+                if nums[mid] < target <= nums[right]:
+                        return _bisect(left = mid + 1,right=right)   # Go right
+                else:
+                        return _bisect(left,right = mid - 1)  # Go left
+            else: # left >= mid >= right 
+                # this mean left = mid = right
+                if _bisect(left,right = mid - 1):  # Go left
+                    return True
+                if _bisect(left = mid + 1,right=right):   # Go right
+                    return True
+                return False
+
+        return _bisect()
